f3d11e23b45d794c821bd1f9c442ef678485b6b8
mohamad naji <mohamadnaji@mohamads-MacBook-Pro.local>
2018-01-10 13:11:17 +0330

    version 0

--- myfirstapp/ViewController.swift
+++ myfirstapp/ViewController.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bgp: UIImageView!
+    @IBOutlet weak var mainp: UIImageView!
+    @IBOutlet weak var gobut: UIButton!
+    @IBOutlet weak var backbut: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        backbut.isHidden = true;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func gotounhidepic(){
+        mainp.isHidden = false;
+        bgp.isHidden = false;
+        gobut.isHidden = true;
+        backbut.isHidden = false;
+    }
+    @IBAction func backtohidden(){
+        mainp.isHidden = true;
+        bgp.isHidden = true;
+        gobut.isHidden = false;
+        backbut.isHidden = true;
     }
 
 
